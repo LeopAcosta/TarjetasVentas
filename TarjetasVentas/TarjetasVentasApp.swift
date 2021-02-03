1844d2dd05682ec3e0b47fff290dc6f9bfd94b00
Leo <leopacosta2@gmail.com>
2021-02-02 18:46:25 -0600

    Initial Commit

---
+++ TarjetasVentas/TarjetasVentasApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarjetasVentasApp.swift
+//  TarjetasVentas
+//
+//  Created by Leop Acosta on 02/02/21.
+//
+
+import SwiftUI
+
+@main
+struct TarjetasVentasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
